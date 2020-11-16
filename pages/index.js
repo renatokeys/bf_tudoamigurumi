8fcc53ee1b9d3192bf8f96e23eeb3a664be4a015
renatokeys <r3nat0hs5@gmail.com>
2020-11-16 20:26:13 -0300

    First commit

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+
+
+export default function Home() {
+  return (
+    <div className="container">
+      <Head>
+        <title>Tudo Amigurumi</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    </div>
+  )
+}
